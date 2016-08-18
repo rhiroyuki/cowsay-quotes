c981cb7bafc3323faba12a93ac7cf2d89e93ed41
rhiroyuki <ricardo.hiroyuki@hotmail.com>
2016-08-18 18:58:13 +0000

    Implementa api e cowsay

---
+++ request.py
@@ -0,0 +1,8 @@
+from urllib2 import urlopen, Request, URLError
+import subprocess
+
+try:
+	request = urlopen("http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en")
+	subprocess.call(["cowsay", request.read()])
+except URLError, e:
+	print("Error.")
